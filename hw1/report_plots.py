6241645ca177bdbd4fc2703626d6c5857a4f1996
ce-hao <ce.hao@tusimple.ai>
2021-09-03 17:45:00 +0800

    it can save

--- hw1/report_plots.py
+++ hw1/report_plots.py
@@ -8,48 +8,57 @@ import os
 # manually add data of Ant and Halfcheeta
 
 
-def plot_mean_std(ax, iterations, mean, std, mean_exp, mean_bc):
-    
+def plot_mean_std(ax, iterations, mean, std, mean_expert, mean_bc):
+    mean = np.array(mean)
+    std = np.array(std)
 
     ax.plot(iterations, mean_expert, 'r', label='expert')
     ax.plot(iterations, mean_bc, 'g', label='naive bc')
     ax.plot(iterations, mean, 'b-s', label='DAgger mean')
     ax.fill_between(iterations, mean-std, mean+std, alpha=0.2, label='DAgger std')
-    
 
 
-def set_plot_env(iterations, mean, std, mean_exp, mean_bc, exp_name):
+def set_plot_env(iterations, mean, std, mean_expert, mean_bc, exp_name):
 
     plt.figure(figsize=(10,5))
     style = "whitegrid"
     sns.set_theme(style=style) # background color
     ax = plt.gca()
-    plot_mean_std(ax, no_iter, mean, std, mean_expert, mean_bc)
+    plot_mean_std(ax, iterations, mean, std, mean_expert, mean_bc)
 
     ax.legend()
     ax.set_xlabel('Iterations')
     ax.set_ylabel('Return')
-    ax.set_title('return of exp')
+    ax.set_title('return of exp ' + exp_name)
 
     exp_dir = 'plots/'
     if not os.path.exists(exp_dir):
         os.makedirs(exp_dir)
     plt.savefig(fname=exp_dir + exp_name + '.svg', format='svg')
 
+def plot_DAgger(mean, std, exp_name):
+    iterations = np.arange(len(mean))
+    I = np.ones(len(mean))
+    mean_bc = mean[0] * I 
+    mean_expert = mean[-1] * I 
+    set_plot_env(iterations, mean, std, mean_expert, mean_bc, exp_name)
 
 class Ant_exp:
-    mean = [1]
-    std =  []
-    mean_bc = mean[0]
-    mean_expert = mean[-1]
-    iterations = np.arange(len(mean))
+    mean = [4274., 4648., 4746., 4619., 4447., 4356., 4731., 4739., 4581., 4834., 4648., 4382., 4750., 4809., 4729., 4718., 4492., 4829., 4676., 4682.]
+    std =  [1128.,   53.,   85.,  103.,  854., 1030.,  124.,  135.,  336.,  109.,  499., 1152.,  91.,   68.,  104.,   72.,  169.,  127.,   81.,  375.]
+
 
 if __name__ == "__main__":
-    no_iter = np.arange(10)
-    mean = np.linspace(0.9, 1.1, 10)
-    std = np.linspace(0, 0.2, 10)
+    # no_iter = np.arange(10)
+    # mean = np.linspace(0.9, 1.1, 10)
+    # std = np.linspace(0, 0.2, 10)
 
-    mean_expert = np.ones(10) * 1.3
-    mean_bc = np.ones(10) * 0.9
+    # mean_expert = np.ones(10) * 1.3
+    # mean_bc = np.ones(10) * 0.9
 
-    set_plot_env(no_iter, mean, std, mean_expert, mean_bc, 'Ant')
+    exp = Ant_exp
+    mean = exp.mean
+    std = exp.std
+
+
+    plot_DAgger(mean, std, 'Ant')
